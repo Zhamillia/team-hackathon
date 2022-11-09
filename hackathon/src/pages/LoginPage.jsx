be9997393b62a59ff4db7a2c710532d48396885f
Zhamilia <Zhamilia@Jamis-MacBook-Air.local>
2022-11-09 21:16:16 +0600

    footer, registration, auzhoritaion

---
+++ hackathon/src/pages/LoginPage.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContextProvider";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { login } = useAuth();
+
+  return (
+    <div id="log-div">
+      <div id="log-form">
+        <h2 id="log-h2">Log In</h2>
+
+        <input
+          className="log-inp"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+
+        <input
+          className="log-inp"
+          type="text"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button id="log-btn" onClick={() => login(username, password)}>
+          Login
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ hackathon/src/pages/PaidPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaidPage = () => {
+  return <div>PaidPage</div>;
+};
+
+export default PaidPage;
