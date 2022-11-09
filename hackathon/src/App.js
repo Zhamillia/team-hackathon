29515076af600def9ed51759f847892e367bc5b6
Aizhamal <aizhamal.batyrbekova@alatoo.edu.kg>
2022-11-09 21:10:30 +0600

    added creatProduct and pagination

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import MainRoutes from "./MainRoutes";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import ProductContextProvider from "./contexts/ProductContextProvider";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <MainRoutes />
-      <Footer />
+      <ProductContextProvider>
+        <Navbar />
+        <MainRoutes />
+        <Footer />
+      </ProductContextProvider>
     </>
   );
 };
--- hackathon/src/pages/ProductsPage.jsx
+++ hackathon/src/pages/ProductsPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import ProductsList from "../components/products/ProductsList/ProductsList";
+// import ProductSideBar from "../components/products/ProductSideBar/ProductSideBar";
+import "../styles/ProductsPage.css";
 
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  const [page, setPage] = useState(1);
+  // const [isSideBar, setIsSideBar] = useState(true);
+
+  // function changeSideBarStatus() {
+  //   setIsSideBar(!isSideBar);
+  // }
+
+  return (
+    <div className="productsPage">
+      {/* <ProductSideBar isSideBar={isSideBar} setPage={setPage} /> */}
+      <ProductsList
+        page={page}
+        setPage={setPage}
+        // changeSideBarStatus={changeSideBarStatus}
+      />
+    </div>
+  );
 };
 
 export default ProductsPage;
